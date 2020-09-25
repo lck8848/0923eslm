d3368128d7c1412f9e6b80b6ccc241a0469e3be1
zkmgit <2821800369@qq.com>
2020-09-25 23:49:34 +0800

    456

--- api/homeApi.js
+++ api/homeApi.js
@@ -3,3 +3,7 @@ import request from './request.js';
 export async function userList(){
 	return await request("/userList",{});
 }
+// 获取商家的数据
+export async function merchantList(){
+	return await request("/merchantList",{});
+}
