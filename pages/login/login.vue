d187e6ca7d48420aeb02c5bf6c29615b1f10764a
lck8848 <1370887376@qq.com>
2020-09-25 14:41:31 +0800

    login update

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -1,22 +1,107 @@
 <template>
 	<view class="container">
-		login
-
+		<view class="login-container">
+			<view class="row-container">
+				<view class="login-img"><image class="img" src="/static/images/home/elm-login.png"></image></view>
+				<van-cell-group>
+					<input class="input" type="text" v-model="username" placeholder="用户名" />
+					<input class="input" type="password" v-model="password" placeholder="密码" />
+				</van-cell-group>
+				<view class="text">
+					新用户登录即自动注册，并表示已同意
+					<text class="blue">《用户服务协议》</text>
+					和
+					<text class="blue">《隐私权政策》</text>
+				</view>
+			</view>
+			<!-- <button type="primary" block>块级元素</button> -->
+			<view class="btn"><button @click="login">登录</button></view>
+			<view class="about">关于我们</view>
+		</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-			}
-		},
-		methods: {
-
+export default {
+	data() {
+		return {
+			username: '',
+			password: ''
+		};
+	},
+	methods: {
+		login() {
+			console.log(this.username, this.password);
 		}
 	}
+};
 </script>
 
 <style lang="scss" scoped>
-	
+/* pages/login/login.wxss */
+.login-container .row-container {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	margin-top: 60rpx;
+	margin-bottom: 20rpx;
+	padding: 0 70rpx;
+}
+
+.login-container .login-img {
+	width: 276rpx;
+	height: 70rpx;
+	margin-bottom: 15rpx;
+}
+
+.login-container .login-img .img {
+	width: 100%;
+	height: 100%;
+}
+
+.login-container .text {
+	font-weight: 400;
+	margin-top: 36rpx;
+	font-size: 28rpx;
+	line-height: 40rpx;
+	// padding: 0 40rpx;
+	color: #999;
+}
+.login-container .text .blue {
+	color: #00acf0;
+}
+.login-container .input {
+	width: 580rpx;
+	height: 96rpx;
+	left: 0;
+	padding-left: 10px;
+	border: 1px solid #ddd;
+	border-radius: 4px;
+	color: #333;
+	margin-top: 20rpx;
+}
+.login-container .btn {
+	width: 580rpx;
+	margin: 0 auto;
+}
+.login-container .btn > button {
+	display: block;
+	width: 100%;
+	height: 42px;
+	margin-top: 30px;
+	border-radius: 21px;
+	background: #02b6fd;
+	color: #fff;
+	cursor: pointer;
+	text-align: center;
+	font-size: 16px;
+	font-weight: 700;
+	line-height: 42px;
+}
+.login-container .about {
+	text-align: center;
+	font-size: 24rpx;
+	margin-top: 20px;
+	color: #999;
+}
 </style>
--- pages/order/order.vue
+++ pages/order/order.vue
@@ -1,38 +1,46 @@
 <template>
 	<view class="container">
 		<view class="order-container">
-		    <view class="nologin">
-		        <image class="img" src="/static/images/home/elm.png"></image>
-		        <view class="title">登录后查看外卖订单</view>
-		        <van-button type="primary" size="small">立即登录</van-button>
-		    </view>
+			<view class="nologin">
+				<image class="img" src="/static/images/home/elm.png"></image>
+				<view class="title">登录后查看外卖订单</view>
+				<button type="primary" size="small" class="btn">立即登录</button>
+			</view>
 		</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-			}
-		},
-		methods: {
-
-		}
-	}
+export default {
+	data() {
+		return {};
+	},
+	methods: {}
+};
 </script>
 
 <style lang="scss" scoped>
-	.order-container .nologin {
-	    display: flex;
-	    flex-direction: column;
-	    justify-content: center;
-	    align-items: center;
-	    margin-top: 200rpx;
-	}
-	
-	.order-container .nologin .img {
-	    width: 500rpx;
-	    height: 500rpx;
-	}
+.order-container .nologin {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	margin-top: 200rpx;
+}
+
+.order-container .nologin .img {
+	width: 500rpx;
+	height: 500rpx;
+}
+
+.order-container .nologin .btn {
+	width: 240rpx;
+	height: 78rpx;
+	line-height: 78rpx;
+	background-color: #56d176;
+	color: #fff;
+	font-size: 28.8rpx;
+	text-align: center;
+	border-radius: 6rpx;
+}
 </style>
